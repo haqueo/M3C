f1ce65ca12bf636d3796195bb39032df3e29cbd2
haqueo <omarhaque@hotmail.co.uk>
2016-12-09 16:43:18 +0000

    Added modules/so files, and finished first version of part1_dev.f90

--- part1/part1_dev.f90
+++ part1/part1_dev.f90
@@ -6,7 +6,7 @@ contains
 
 
 
-subroutine rwnet(Ntime,m,X0,N0,L,Nt,isample,X,XM)
+subroutine rwnet(Ntime,Nm,X0,N0,L,Nt,isample,X,XM)
     !random walks on a recursive network
     !Input variables:
     !Ntime: number of time steps
@@ -17,10 +17,16 @@ subroutine rwnet(Ntime,m,X0,N0,L,Nt,isample,X,XM)
     !XM: fraction of walks at initial node, computed every isample timesteps
 	implicit none
     integer, intent(in) :: Ntime,Nm,N0,L,Nt,X0,isample
-    integer, dimension(Ntime+1,Nm), intent(out) :: X
-    integer :: i1,qmax
+    integer, dimension(Ntime+1,Nm), intent(out) :: X,XM
+    integer :: i1,qmax, node,lowerBound,upperBound,degNode,increment, counter,j
     integer, dimension(N0+Nt) :: qnet
-    integer, dimension(N0+L*Nt,2) :: enet 
+    integer, dimension(N0+L*Nt,2) :: enet
+    integer, dimension((N0+L*Nt)*2) :: alist1
+    integer, dimension(N0+Nt) :: alist2
+    real(kind=8) :: u,current
+
+    
+     
     
     !A walker is at node Xi at time, ti
     !at time ti+1 it will move to one of the adjacent nodes
@@ -32,23 +38,96 @@ subroutine rwnet(Ntime,m,X0,N0,L,Nt,isample,X,XM)
     
     !add code to compute XM every isample time steps
     
+    !find the index in qnet with value qmax
+    
+    
+    !generate the network
     call generate(N0,L,Nt,qmax,qnet,enet)
+    
+    !convert qnet, enet into alist1, alist2
+    call adjacency_list(qnet,enet,alist1,alist2)
+    
+    !Define the initial node
+    node = X0
+    IF (node .eq. 0) THEN
+        do i1 = 1, N0+Nt
+            if (qnet(i1) .eq. qmax) then
+                node = i1
+                exit
+            end if
+        end do 
+    END IF
+    
+    !complete a single walk
+    
+    !initialise first row of X
+    X(1,:) = node
+    
+    DO j = 1,Nm
+    
+    
+        DO i1 = 1,Ntime
+            !!!GIVEN SUBLIST FUNCTION(node) !!!!
+            
+            !! GENERATE THE SUBLIST!!!
+            !! lower bound is alist1(node) 
+            !! upper bound is alist2(node+1)
+            !!unless node is the final node. then it's alist1(node) till 
+            !the last value of alist2
+            
+            lowerBound = alist1(node)
+            
+            
+            IF (node .eq. N0+NT) THEN
+                    upperBound = size(alist2) ! <- we know what this size is
+            ELSE 
+                    upperBound = alist1(node+1)
+            END IF
+            
+            
+            !now we have our sublist.
+            
+            degNode = size(alist2(lowerBound:upperBound))
+            increment = dble(1)/(dble(degNode))
+            
+            call random_number(u)
+            counter = 0
+            current = 0
+            
+            DO WHILE (current .lt. u)
+            
+                current = current + increment
+                counter = counter + 1
+                
+            END DO
+            
+            node = alist2(lowerBound + counter -1)
+            !this is the next node
+            !so add it to 
+            
+            X(1+i1,j) = node
+        
+        
+        END DO
+        
+    END DO
+    
     
 
 
 end subroutine rwnet
 
 
-subroutine rwnet_omp(Ntime,m,X0,N0,L,Nt,isample,numthreads,X,XM)
+subroutine rwnet_omp(Ntime,Nm,X0,N0,L,Nt,isample,numthreads,X,XM)
     !parallelized version of rwnet, parallel regions should
     !use numthreads threads
 	implicit none
     integer, intent(in) :: Ntime,Nm,N0,L,Nt,X0,isample,numthreads
-    integer, dimension(Ntime+1,Nm), intent(out) :: X
+    integer, dimension(Ntime+1,Nm), intent(out) :: X,XM
 
 
 
-end subroutine rwnet
+end subroutine rwnet_omp
 
 
 end module rwmodule
--- part1/test.py
+++ part1/test.py
@@ -1,7 +1,12 @@
 from networktest import network
+from rwnettest import rwmodule
 
-qmax,qnet,enet = network.generate(12,4,8)
+
+
+qmax,qnet,enet = network.generate(5,2,4)
 print enet
 alist1, alist2 = network.adjacency_list(qnet,enet)
 print alist1
+print alist2
 
+print len(alist1)
